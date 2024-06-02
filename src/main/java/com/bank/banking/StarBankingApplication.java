d506ec09c0ef496a97bb840f20b3b0f5e75cd069
shipra sinha <shiprashikta@gmail.com>
2024-06-01 18:05:58 -0700

    Banking Project Structure

---
+++ src/main/java/com/bank/banking/StarBankingApplication.java
@@ -0,0 +1,13 @@
+package com.bank.banking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarBankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarBankingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bank/banking/StarBankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.banking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarBankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
